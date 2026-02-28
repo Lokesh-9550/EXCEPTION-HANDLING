a591f2c8f1e9ece16a7da8af3bb77383193b410e
Lokesh <puttamlokesh9550@gmail.com>
2026-02-28 12:38:48 +0530

    Added Throw key word

---
+++ Task4.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Alpha{
+    Scanner sc=new Scanner(System.in);
+    public void calc() throws ArithmeticException{
+        
+        
+        System.out.println("Alpha connection is created");
+        System.out.println("Enter numaratoe");
+        int n1=sc.nextInt();
+        System.out.println("Enter Dinominator");
+        int n2=sc.nextInt();
+        int res=n1/n2;
+        System.out.println("Result is: "+res);
+        System.out.println("Alpha connection is terminated");
+       
+    }
+}
+
+public class Task4 {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Main connection is created");
+            Alpha a=new Alpha();
+            a.calc();
+            System.out.println("Main connection is terminated");
+        }
+        catch(ArithmeticException e){
+            System.out.println("Exception is caught in main method");
+
+        }
+    }
+    
+}
