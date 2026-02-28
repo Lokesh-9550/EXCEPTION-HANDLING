3c921979df21890a1e1caa2d6584b5a84b137719
Lokesh <puttamlokesh9550@gmail.com>
2026-02-28 11:31:47 +0530

    Task2

---
+++ Task2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Connection is created");
+        try{
+        System.out.println("Enter numaratoe");
+        int n1=sc.nextInt();
+        System.out.println("Enter Dinominator");
+        int n2=sc.nextInt();
+        int res=n1/n2;
+        System.out.println("Result is: "+res);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Do not Enter zero values in diverder");
+        }
+        try{
+        System.out.println("Enter size of Array");
+        int size=sc.nextInt();
+        int [] arr=new int[size];
+        System.out.println("Enter the Element in a Array");
+        int ele=sc.nextInt();
+        System.out.println("Enter Which position to Enter Array value ");
+        int pos=sc.nextInt();
+        arr[pos]=ele;
+        System.out.println("Data is Entered and value is "+arr[pos]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("enetr values in with in the array");
+        }
+        catch(NegativeArraySizeException e){
+            System.out.println("Do not enter negative value sin array");
+        }
+        
+        catch(Exception e){
+            System.out.println("some of problem");
+
+        }
+        System.out.println("Connection is terminated");
+    }
+    
+    
+}
